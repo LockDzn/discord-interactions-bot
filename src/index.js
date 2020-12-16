0eae59a23645dd87d9ec3eced95e11bad87fa1b5
Ryan Souza <ryanssouza00@gmail.com>
2020-12-16 01:23:37 -0300

    adding registerCommand function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const axios = require('axios');
 const { InteractionType, InteractionResponseType, verifyKeyMiddleware, verifyKey } = require('discord-interactions');
+
+const discordAPI = axios.create({
+  baseURL: 'https://discord.com/api/v8',
+  headers: {
+    'authorization': `Bot ${process.env.DISCORD_BOT_TOKEN}`
+  }
+})
 
 const port = process.env.PORT || 3999;
 
 const app = express();
+
+async function registerCommand() {
+  await discordAPI.post(`/applications/${process.env.CLIENT_ID}/commands`, {
+    name: 'ping',
+    description: 'Command to return bot status',
+    options: []
+  })
+}
 
 app.post('/interactions', verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY), async (req, res) => {
   const signature = req.get('X-Signature-Ed25519');
@@ -32,4 +48,5 @@ app.post('/interactions', verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY), as
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
+  registerCommand()
 });
