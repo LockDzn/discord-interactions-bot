3a4ee89cdae0a7e0b33d2442a6772409e0952b3f
Ryan Souza <ryanssouza00@gmail.com>
2020-12-16 13:14:15 -0300

    updating

--- src/utils/commandsLoader.js
+++ src/utils/commandsLoader.js
@@ -1,31 +1,36 @@
 const fs = require('fs');
 
-module.exports = () => {
-    const commands = new Map();
-    const aliases = new Map();
+const commands = new Map();
+const aliases = new Map();
 
-    fs.readdir("./commands", (err, files) => {
-        if(err) console.error(err);
-        files.forEach((f, i) => {
-            let folder = f.split('.');
-            if(folder[1]) return;
-            fs.readdir(`./commands/${f}/`, (err, jsf) => {
-                let jsfiles = jsf.filter(f => f.split(".").pop() === "js");
-                if(jsfiles.length <= 0 ) {
-                    return;
-                }
-                jsfiles.forEach((j, k) => {
-                    let props = require(`../commands/${f}/${j}`);
-                    props.help.type = f;
-                    client.commands.set(props.help.name, props);
-                    if(!props.help || !props.help.aliases || props.help.aliases[0] == '') return;
-                    props.help.aliases.forEach(alias => {
-                        client.aliases.set(alias, props.help.name);
+fs.readdir("./commands", (err, files) => {
+    if(err) console.error(err);
+
+    files.forEach((f, i) => {
+        let folder = f.split('.');
+
+        if(folder[1]) return;
+
+        fs.readdir(`./commands/${f}/`, (err, jsf) => {
+
+            let jsfiles = jsf.filter(f => f.split(".").pop() === "js");
+
+            if(jsfiles.length <= 0 ) return;
+            
+            jsfiles.forEach((j, k) => {
+                let props = require(`../commands/${f}/${j}`);
+                props.help.type = f;
+
+                commands.set(props.help.name, props);
+
+                if(!props.help || !props.help.aliases || props.help.aliases[0] == '') return;
+
+                props.help.aliases.forEach(alias => {
+                    aliases.set(alias, props.help.name);
                 })
             });
-            })
         })
-    });
+    })
+});
 
-    return { commands, aliases }
-}
+module.exports = { commands, aliases };
