97523ba04484eb1edf556c6db99edf38cc5820af
Ryan Souza <ryanssouza00@gmail.com>
2020-12-16 01:28:51 -0300

    adding public key to verify

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ async function registerCommand() {
 app.post('/interactions', verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY), async (req, res) => {
   const signature = req.get('X-Signature-Ed25519');
   const timestamp = req.get('X-Signature-Timestamp');
-  const isValidRequest = await verifyKey(req.rawBody, signature, timestamp, 'MY_CLIENT_PUBLIC_KEY');
+  const isValidRequest = await verifyKey(req.rawBody, signature, timestamp, process.env.CLIENT_PUBLIC_KEY);
   if (!isValidRequest) {
     return res.status(401).end('Bad request signature');
   }
