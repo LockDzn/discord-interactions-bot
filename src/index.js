ec43548e7b271e67457071835978087ad7fff101
Ryan Souza <ryanssouza00@gmail.com>
2020-12-16 01:11:20 -0300

    redoing the code

--- src/index.js
+++ src/index.js
@@ -5,29 +5,6 @@ const port = process.env.PORT || 3999;
 
 const app = express();
 
-async function handleCommand(data, res) {
-  switch (data.name) {
-    case 'ping':
-      res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE,
-        data: {
-          content: "Hello World",
-          flags: InteractionResponseFlags.EPHEMERAL,
-        },
-      });
-      break;
-    default:
-      res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE,
-        data: {
-          content: "Sorry, I don't understand this command :(",
-        },
-        flags: InteractionResponseFlags.EPHEMERAL,
-      });
-      break;
-  }
-}
-
 app.post('/interactions', verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY), async (req, res) => {
   const signature = req.get('X-Signature-Ed25519');
   const timestamp = req.get('X-Signature-Timestamp');
@@ -39,9 +16,19 @@ app.post('/interactions', verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY), as
   const interaction = req.body;
   console.log(interaction)
   if (interaction.type === InteractionType.COMMAND) {
-    handleCommand(interaction.data, res);
+    if (interaction.data.name == "ping") {
+      res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          embeds: [{
+            title: 'Testing',
+            description: 'pong!'
+          }]
+        }
+      })
+    }
   }
-});;
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
