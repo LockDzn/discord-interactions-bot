ca9242d5351563e6a7fab54e41101ec951d44144
Ryan Souza <ryanssouza00@gmail.com>
2020-12-16 01:07:01 -0300

    adding cosole.log

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ async function handleCommand(data, res) {
   }
 }
 
-app.post('/interactions', verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY), (req, res) => {
+app.post('/interactions', verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY), async (req, res) => {
   const signature = req.get('X-Signature-Ed25519');
   const timestamp = req.get('X-Signature-Timestamp');
   const isValidRequest = await verifyKey(req.rawBody, signature, timestamp, 'MY_CLIENT_PUBLIC_KEY');
@@ -37,6 +37,7 @@ app.post('/interactions', verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY), (r
   }
 
   const interaction = req.body;
+  console.log(interaction)
   if (interaction.type === InteractionType.COMMAND) {
     handleCommand(interaction.data, res);
   }
