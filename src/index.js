bcf4ac3c140aebca7238d2001610deb6a18e1ce8
Ryan Souza <ryanssouza00@gmail.com>
2020-12-16 16:21:01 -0300

    updating

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { InteractionType, verifyKeyMiddleware, verifyKey } = require('discord-interactions');
 
-const commands = require('./utils/commandsLoader')
+const commandsLoader = require('./utils/commandsLoader')
 
 const port = process.env.PORT || 3999;
 
@@ -11,8 +11,8 @@ app.post('/interactions', verifyKeyMiddleware(process.env.CLIENT_PUBLIC_KEY), as
     const message = req.body;
     // console.log(message)
     if (message && message.type === InteractionType.COMMAND) {
-        const command = commands.commands.get(message.data.name) ||
-        commands.commands.get(commands.aliases.get(message.data.name));
+        const command = commandsLoader.commands.get(message.data.name) ||
+        commandsLoader.commands.get(commandsLoader.aliases.get(message.data.name));
 
         if(!command) return;
 
